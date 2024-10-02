a82e131df6e3e9865a0deeb8973dd7b3cfe8690a
muhammadibrahim <mik237@gmail.com>
2024-10-02 23:10:29 +0400

    code cleanup

--- app/src/main/java/me/ibrahim/composepractice/main/MainActivity.kt
+++ app/src/main/java/me/ibrahim/composepractice/main/MainActivity.kt
@@ -7,7 +7,6 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
-import me.ibrahim.composepractice.list_detail_pane.ListDetailLayout
 import me.ibrahim.composepractice.ui.theme.ComposePracticeTheme
 
 class MainActivity : ComponentActivity() {
