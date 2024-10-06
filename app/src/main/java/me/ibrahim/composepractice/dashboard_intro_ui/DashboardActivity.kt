1d616aa3a97f631d6098f7b9cf7a4264ec783ff8
muhammadibrahim <mik237@gmail.com>
2024-10-06 21:57:50 +0400

    Dashboard content screen added with bottom navigation, profile image and two cards.

--- app/src/main/java/me/ibrahim/composepractice/dashboard_intro_ui/DashboardActivity.kt
+++ app/src/main/java/me/ibrahim/composepractice/dashboard_intro_ui/DashboardActivity.kt
@@ -4,10 +4,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import me.ibrahim.composepractice.R
 
 class DashboardActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
