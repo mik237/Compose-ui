cef629da59f370ac1839559a7a54b0c922af9934
muhammadibrahim <mik237@gmail.com>
2024-10-23 22:31:02 +0400

    EEUtil.kt: encryption/decryption using AES algorithm

--- app/src/main/java/me/ibrahim/composepractice/utils/EEUtil.kt
+++ app/src/main/java/me/ibrahim/composepractice/utils/EEUtil.kt
@@ -15,6 +15,9 @@ import java.security.KeyStore
 import java.security.PrivateKey
 import java.util.Calendar
 import javax.crypto.Cipher
+import javax.crypto.KeyGenerator
+import javax.crypto.SecretKey
+import javax.crypto.spec.IvParameterSpec
 import javax.security.auth.x500.X500Principal
 
 object EEUtil {
@@ -23,15 +26,25 @@ object EEUtil {
     private const val ASYMMETRIC_KEY_ALIAS = "keyPairForRSA"
     private const val KEYSTORE_TYPE = "AndroidKeyStore"
     private const val RSA_ALGORITHIM = "RSA"
+    private const val AES_ALGORITHIM = "AES"
 
-    private const val TRANSFORMATION_ASYMMETRIC = "$RSA_ALGORITHIM/ECB/PKCS1Padding"
+    private const val TRANSFORMATION_ASYMMETRIC = "$RSA_ALGORITHIM/${KeyProperties.BLOCK_MODE_ECB}/PKCS1Padding"
+    private const val TRANSFORMATION_SYMMETRIC = "$AES_ALGORITHIM/${KeyProperties.BLOCK_MODE_CBC}/${KeyProperties.ENCRYPTION_PADDING_PKCS7}"
 
 
     private val keyStore: KeyStore = createAndroidKeyStore()
     val asymmetricKey: KeyPair = createKeyPair()
-    val cipher = Cipher.getInstance(TRANSFORMATION_ASYMMETRIC)
+    val symmetricKey: SecretKey = createSymmetricKey()
+
+    //Create & load KeyStore (AndroidKeyStore)
+    private fun createAndroidKeyStore(): KeyStore {
+        val keyStore = KeyStore.getInstance(KEYSTORE_TYPE)
+        keyStore.load(null)
+        return keyStore
+    }
 
 
+    //get keys previously stored in key store.
     fun getPublicAndPrivateKeysFromKeyStore(): KeyPair? {
         val privateKey = keyStore.getKey(ASYMMETRIC_KEY_ALIAS, null) as? PrivateKey
         val publicKey = keyStore.getCertificate(ASYMMETRIC_KEY_ALIAS).publicKey
@@ -39,6 +52,12 @@ object EEUtil {
         return if (publicKey != null && privateKey != null)
             KeyPair(publicKey, privateKey)
         else null
+    }
+
+    //get AES symmetric key from android key store.
+    fun getAESKeyFromKeyStore(): SecretKey? {
+        val secretKey = keyStore.getKey(SYMMETRIC_KEY_ALIAS, null) as? SecretKey
+        return secretKey
     }
 
     //this will create KeyPair (public & private keys for RSA algorithms)
@@ -55,6 +74,18 @@ object EEUtil {
         // Generates Key with given spec and also saves it to the KeyStore
         //* In Android Key Store provider, this method will automatically save key in KeyStore
         return keyGenerator.generateKeyPair()
+    }
+
+
+    fun createSymmetricKey(): SecretKey {
+        val keyGenerator = KeyGenerator.getInstance(KeyProperties.KEY_ALGORITHM_AES, KEYSTORE_TYPE)
+        val builder = KeyGenParameterSpec.Builder(SYMMETRIC_KEY_ALIAS, KeyProperties.PURPOSE_ENCRYPT or KeyProperties.PURPOSE_DECRYPT)
+            .setBlockModes(KeyProperties.BLOCK_MODE_CBC)
+            .setEncryptionPaddings(KeyProperties.ENCRYPTION_PADDING_PKCS7)
+            .setKeySize(256)
+
+        keyGenerator.init(builder.build())
+        return keyGenerator.generateKey()
     }
 
     //region initialize KeyPairGenerator
@@ -83,24 +114,61 @@ object EEUtil {
 
     //endregion
 
-    private fun createAndroidKeyStore(): KeyStore {
-        val keyStore = KeyStore.getInstance(KEYSTORE_TYPE)
-        keyStore.load(null)
-        return keyStore
-    }
-
 
     fun encrypt(plainData: String, key: Key): String {
+        val cipher = Cipher.getInstance(TRANSFORMATION_ASYMMETRIC)
         cipher.init(Cipher.ENCRYPT_MODE, key)
         val bytes = cipher.doFinal(plainData.toByteArray())
         return Base64.encodeToString(bytes, Base64.DEFAULT)
     }
 
     fun decrypt(encryptedData: String, key: Key): String {
+        val cipher = Cipher.getInstance(TRANSFORMATION_ASYMMETRIC)
         cipher.init(Cipher.DECRYPT_MODE, key)
         val bytes = Base64.decode(encryptedData, Base64.DEFAULT)
         val data = cipher.doFinal(bytes)
         return String(data)
     }
 
+
+    //region Key Wrapping
+    // encrypting AES key to be stored locally or transfer is called key wrapping
+    fun wrapKey(keyToBeWraped: Key, keyToWrapWith: Key): String {
+        val cipher = Cipher.getInstance(TRANSFORMATION_ASYMMETRIC)
+        cipher.init(Cipher.WRAP_MODE, keyToWrapWith)
+        val wrappedKey = cipher.wrap(keyToBeWraped)
+        return Base64.encodeToString(wrappedKey, Base64.DEFAULT)
+    }
+
+    fun unWrapKey(wrappedKey: String, algorithm: String, wrappedKeyType: Int, keyToUnWrapWith: Key) {
+        val cipher = Cipher.getInstance(TRANSFORMATION_ASYMMETRIC)
+        cipher.init(Cipher.UNWRAP_MODE, keyToUnWrapWith)
+        val wrappedKeyBytes = Base64.decode(wrappedKey, Base64.DEFAULT)
+        val unWrappedKey = cipher.unwrap(wrappedKeyBytes, algorithm, wrappedKeyType)
+    }
+    //endregion
+
+    fun encryptAES(plainData: String, key: Key): String {
+        val cipher = Cipher.getInstance(TRANSFORMATION_SYMMETRIC)
+        cipher.init(Cipher.ENCRYPT_MODE, key)
+        val iv = cipher.iv
+        val encryptedData = cipher.doFinal(plainData.toByteArray())
+        val ivStr = Base64.encodeToString(iv, Base64.DEFAULT)
+        val dataStr = Base64.encodeToString(encryptedData, Base64.DEFAULT)
+        return "$ivStr:$dataStr"
+    }
+
+    fun decryptAES(base64EncodedData: String, key: Key): String {
+        val cipher = Cipher.getInstance(TRANSFORMATION_SYMMETRIC)
+        val dataParts = base64EncodedData.split(":")
+        val ivStr = dataParts[0]
+        val iv = IvParameterSpec(Base64.decode(ivStr, Base64.DEFAULT))
+
+        cipher.init(Cipher.DECRYPT_MODE, key, iv)
+
+        val dataStr = dataParts[1]
+        val encodedBytes = Base64.decode(dataStr, Base64.DEFAULT)
+        val decodedBytes = cipher.doFinal(encodedBytes)
+        return String(decodedBytes)
+    }
 }
